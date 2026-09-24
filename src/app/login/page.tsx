a57587ef25206ee9e4ce06e5e4461831a850b88e
ebtikaradv20-cell <ebtikar.adv20@gmail.com>
2026-09-24 16:51:08 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -93,7 +93,7 @@ export default function LoginPage() {
               </div>
               <h1 className="text-4xl font-black text-white tracking-wider">FLEET<span style={{ color: "#F97316" }}>360</span></h1>
               <p className="text-blue-200 text-sm mt-1">تطبيق إدارة الأسطول الشامل</p>
-              <p className="text-blue-300 text-xs mt-1 font-semibold">TRANSCAS / TAQA ARABIA</p>
+              <p className="text-blue-300 text-xs mt-1 font-semibold">TRANSGAS / TAQA ARABIA</p>
             </div>
 
             <form onSubmit={handleLogin} className="space-y-4">
